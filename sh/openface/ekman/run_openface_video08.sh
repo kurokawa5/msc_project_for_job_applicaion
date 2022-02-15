ebe82a11b934a0de1ee2bd4772f02ee77d5591f2
Kurokawa <76276773+kurokawa5@users.noreply.github.com>
2022-02-15 19:11:45 +0900

    Add files via upload

---
+++ sh/openface/ekman/run_openface_video08.sh
@@ -0,0 +1,22 @@
+#OpenFace
+
+for dir in /content/drive/MyDrive/05_workspace/01_msc_project/data/output/05_cutting_out_videos/ekman/video08/*
+do
+    #Get the emotion category name
+    dirname=`basename $dir`
+
+    for file in /content/drive/MyDrive/05_workspace/01_msc_project/data/output/05_cutting_out_videos/ekman/video08/$dirname/*.mp4
+    do
+        echo $file
+        echo /content/drive/MyDrive/05_workspace/01_msc_project/data/output/06_openface/ekman/video08/$dirname
+        ./OpenFace/build/bin/FaceLandmarkVidMulti -f $file -out_dir /content/drive/MyDrive/05_workspace/01_msc_project/data/output/06_openface/ekman/video08/$dirname
+        
+        # convert the result into MP4
+        filename=`basename $file .mp4`
+        
+        #echo $filename
+        echo /content/drive/MyDrive/05_workspace/01_msc_project/data/output/06_openface/ekman/video08/$dirname/$filename.avi
+        echo /content/drive/MyDrive/05_workspace/01_msc_project/data/output/06_openface/ekman/video08/$dirname/$filename.mp4
+        ffmpeg -y -loglevel info -i /content/drive/MyDrive/05_workspace/01_msc_project/data/output/06_openface/ekman/video08/$dirname/$filename.avi /content/drive/MyDrive/05_workspace/01_msc_project/data/output/06_openface/ekman/video08/$dirname/$filename.mp4
+    done
+done
---
+++ sh/openpose/ekman/run_openpose_video09.sh
@@ -0,0 +1,24 @@
+videodir="video09"
+for dir in /content/drive/MyDrive/05_workspace/01_msc_project/data/output/05_cutting_out_videos/ekman/$videodir/*
+do
+    dirname=`basename $dir`
+    #echo $dir
+    mkdir -p /content/drive/MyDrive/05_workspace/01_msc_project/data/output/09_openpose/ekman/$videodir/$dirname
+    for file in /content/drive/MyDrive/05_workspace/01_msc_project/data/output/05_cutting_out_videos/ekman/$videodir/$dirname/*.avi
+    do
+        filename=`basename $file .avi`
+        echo $filename
+        #inputvideo=/content/drive/MyDrive/midori/output/04_cutting_out_videos/video02/$dirname/$filename.avi
+        #echo $inputvideo
+        outputjson=/content/drive/MyDrive/05_workspace/01_msc_project/data/output/09_openpose/ekman/$videodir/$dirname/$filename/json
+        echo $outputjson
+        mkdir -p $outputjson
+        outputavi=/content/drive/MyDrive/05_workspace/01_msc_project/data/output/09_openpose/ekman/$videodir/$dirname/$filename.avi
+        /content/openpose/build/examples/openpose/openpose.bin --video $file --display 0 --write_video $outputavi --write_json  $outputjson --hand --face
+        # convert the result into MP4
+        
+        #echo $filename
+        outputmp4=/content/drive/MyDrive/05_workspace/01_msc_project/data/output/09_openpose/ekman/$videodir/$dirname/$filename.mp4
+        ffmpeg -y -loglevel info -i $outputavi $outputmp4
+    done
+done
